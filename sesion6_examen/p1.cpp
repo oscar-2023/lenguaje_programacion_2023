b178e4abfba1392aae44430dfa98a4a62e9113d5
oscar-2023 <154532574+oscar-2023@users.noreply.github.com>
2024-01-22 16:36:16 -0500

    Update p1.cpp

--- sesion6_examen/p1.cpp
+++ sesion6_examen/p1.cpp
@@ -2,17 +2,17 @@
 //y por cada respuesta en blanco 0 ;se pide calcular el puntaje total del postulante de una prueba de 20 preguntas.
 #include <iostream>
 using namespace std;
-
 int main() {
     const int totalPreguntas = 20;
     int puntaje = 0;
-
+// inicia en 1 , condion menor igual +1
     for (int i = 1; i <= totalPreguntas; ++i) {
         char respuesta;
+        //entrada de datos
         cout << "Ingrese la respuesta (C: correcta, I: incorrecta, B: en blanco) para la pregunta " << i << ": ";
         cin >> respuesta;
 
-        
+         // casos segun la condicon
         switch (respuesta) {
             case 'C':
             case 'c':
@@ -33,7 +33,7 @@ int main() {
                 break;
         }
     }
-
+ //mostrar en patalla
     cout << "El puntaje total del postulante es: " << puntaje << endl;
 
     return 0;

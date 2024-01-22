613a761733aa56b36a4c6b4917a717fee254fd52
oscar-2023 <154532574+oscar-2023@users.noreply.github.com>
2024-01-22 16:59:37 -0500

    Update p3.cpp

--- sesion6_examen/p3.cpp
+++ sesion6_examen/p3.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-    const int produccionMinimaDiaria = 100;
-    const int incentivoPorUnidad = 10;
-    const int diasLaborables = 6; //lunes a sabado
+     int produccionMinimaDiaria = 100;
+     int incentivoPorUnidad = 10;
+     int diasLaborables = 6; //lunes a sabado // declaracion de variables
 
     int produccionDiaria;
     int incentivoTotal = 0;
-
+// inicializacion , condicion de dias laborales que son 6 dias de lunes a sabado
     
     for (int i = 0; i < diasLaborables; ++i) {
         cout << "Ingrese la producción para el día " << i + 1 << ": ";
         cin >> produccionDiaria;
-
+//comparar la produccionMinimaDiaria
         
         if (produccionDiaria >= produccionMinimaDiaria) {
             incentivoTotal += produccionDiaria * incentivoPorUnidad;
@@ -22,7 +22,7 @@ int main() {
             cout << "La producción no alcanza el mínimo requerido." << endl;
         }
     }
-
+//mostrar en pantalla
     
     cout << "El incentivo total para la semana es: $" << incentivoTotal << endl;
 

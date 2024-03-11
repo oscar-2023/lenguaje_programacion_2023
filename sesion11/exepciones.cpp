25be1d98db2924555e4595862b5f6c7214bfe507
oscar-2023 <154532574+oscar-2023@users.noreply.github.com>
2024-03-11 18:43:01 -0500

    Update exepciones.cpp

--- sesion11/exepciones.cpp
+++ sesion11/exepciones.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int divisor = 0;
+    int dividendo = 10;
 
+    try {
+        if (divisor == 0) {
+            throw "División por cero"; // Lanza una excepción de tipo const char*
+        }
+        int resultado = dividendo / divisor;
+        cout << "Resultado: " << resultado << endl;
+    } catch (const char* mensaje) {
+        cerr << "Excepción atrapada: " << mensaje << endl;
+    }
+
+    return 0;
+}

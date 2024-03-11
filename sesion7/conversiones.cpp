e453008253118f640d5b18fa553e6f15d2f2787e
oscar-2023 <154532574+oscar-2023@users.noreply.github.com>
2024-03-11 18:19:54 -0500

    Update conversiones.cpp

--- sesion7/conversiones.cpp
+++ sesion7/conversiones.cpp
@@ -6,15 +6,15 @@ int main() {
     int entero = 5;
     float flotante = entero; // Conversión implícita de entero a flotante
 
-    std::cout << "Conversión implícita: entero a flotante" << std::endl;
-    std::cout << "Entero: " << entero << ", Flotante: " << flotante << std::endl;
+    cout << "Conversión implícita: entero a flotante" << endl;
+    cout << "Entero: " << entero << ", Flotante: " << flotante << endl;
 
     // Conversión explícita: de flotante a entero
     float flotante2 = 3.14;
     int entero2 = static_cast<int>(flotante2); // Conversión explícita de flotante a entero
 
-    std::cout << "\nConversión explícita: flotante a entero" << std::endl;
-    std::cout << "Flotante: " << flotante2 << ", Entero: " << entero2 << std::endl;
+    cout << "\nConversión explícita: flotante a entero" << endl;
+    cout << "Flotante: " << flotante2 << ", Entero: " << entero2 << endl;
 
     return 0;
 }

9b09ffdd0a27b01e94f9945fc8c8e5afa8d5a03a
oscar-2023 <154532574+oscar-2023@users.noreply.github.com>
2024-03-11 20:14:31 -0500

    Update vehiculo.cpp

--- sesion11/vehiculo.cpp
+++ sesion11/vehiculo.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+// Interfaz Vehiculo
+class Vehiculo {
+public:
+    virtual void acelerar() = 0;
+    virtual void frenar() = 0;
+};
+
+// Clase Automovil que implementa la interfaz Vehiculo
+class Automovil : public Vehiculo {
+public:
+    void acelerar() override {
+        cout << "Automóvil acelerando..." << endl;
+    }
+
+    void frenar() override {
+        cout << "Automóvil frenando..." << endl;
+    }
+};
+
+int main() {
+    Automovil auto1;
+    auto1.acelerar();
+    auto1.frenar();
+
+    return 0;
+}
 
